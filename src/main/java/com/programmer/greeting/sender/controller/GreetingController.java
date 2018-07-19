86efeb5a749f3b5b93893c08c8079029194485de
Vaibhavdeo <vaibhavdeore5@gmail.com>
2018-07-19 17:47:57 +0530

    Update GreetingController.java

--- src/main/java/com/programmer/greeting/sender/controller/GreetingController.java
+++ src/main/java/com/programmer/greeting/sender/controller/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.programmer.greeting.exception.UserNotFoundException;
 import com.programmer.greeting.sender.beans.GreetingDetails;
 import com.programmer.greeting.sender.beans.ResponseMessgae;
