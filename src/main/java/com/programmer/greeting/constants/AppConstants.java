fd3c21fe5cfad55f4f1afbec6a640030680b501b
Vaibhavdeo <vaibhavdeore5@gmail.com>
2018-07-31 10:42:36 +0530

    Update AppConstants.java

--- src/main/java/com/programmer/greeting/constants/AppConstants.java
+++ src/main/java/com/programmer/greeting/constants/AppConstants.java
@@ -6,6 +6,9 @@ public class AppConstants {
 	public static final String GET_DETAILS_QUERY = "select * from GreetingDetails where G_ID = ?";
 	public static final String GET_ALL_DETAILS_QUERY = "select * from GreetingDetails";
 	public static final String DELETE_QUERY = "delete from GreetingDetails where g_Id = ?";
-	public static final String UPDATE_QUERY = "update GreetingDetails set SP_DATE = ?, name = ?, subject=?, mail_Id=? where g_Id = ? ";
+	public static final String UPDATE_QUERY = "IF EXISTS (SELECT * FROM GreetingDetails WHERE g_Id=? )
+    UPDATE GreetingDetails SET (SP_DATE = ?, name = ?, subject=?, mail_Id=?) WHERE g_Id=?
+ELSE
+    INSERT INTO Table1 VALUES (g_Id, SP_DATE, subject ,name, mail_Id) values (?, ? , ? , ?, ?)";
 
 }
