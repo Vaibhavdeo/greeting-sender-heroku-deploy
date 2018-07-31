a60d574e18c82cb5484109404b2b940dd1f06d5d
Vaibhavdeo <vaibhavdeore5@gmail.com>
2018-07-31 11:00:02 +0530

    Update AppConstants.java

--- src/main/java/com/programmer/greeting/constants/AppConstants.java
+++ src/main/java/com/programmer/greeting/constants/AppConstants.java
@@ -5,19 +5,5 @@ public class AppConstants {
 	public static final String INSERT_QUERY = "insert into GreetingDetails(g_Id, SP_DATE, subject ,name, mail_Id) values (?, ? , ? , ?, ?)";
 	public static final String GET_DETAILS_QUERY = "select * from GreetingDetails where G_ID = ?";
 	public static final String GET_ALL_DETAILS_QUERY = "select * from GreetingDetails";
-	public static final String DELETE_QUERY = "delete from GreetingDetails where g_Id = ?";
-	public static final String UPDATE_QUERY = " IF EXISTS (
-  SELECT
-    *
-  FROM
-    GreetingDetails
-  WHERE
-    g_Id = ?
-)
-UPDATE
-  GreetingDetails SET (SP_DATE = ?, name = ?, subject = ?, mail_Id = ?) WHERE g_Id = ? 
-ELSE
-INSERT INTO GreetingDetails VALUES(g_Id, SP_DATE, subject, name, mail_Id)
-values (?, ?, ?, ?, ?)"
-
+	public static final String DELETE_QUERY = " IF EXISTS( SELECT * FROM GreetingDetails WHERE g_Id = ? ) UPDATE GreetingDetails SET (SP_DATE = ?, name = ?, subject = ?, mail_Id = ?) WHERE g_Id = ? ELSE INSERT INTO GreetingDetails VALUES(g_Id, SP_DATE, subject, name, mail_Id) values (?, ?, ?, ?, ?)";
 }
