5adcf3a9c0b954373fae2c43a3c2e22b67bb0b50
vaibhav.deore <vaibhav.deore@P5-H7WYXC2.dir.svc.accenture.com>
2018-10-25 15:56:08 +0530

    Added new end point for event display.
    Change-Id: I93c80fa07bb4f5619edf3117c2ee2d08fd336d8a

--- src/main/java/com/programmer/greeting/sender/controller/GreetingController.java
+++ src/main/java/com/programmer/greeting/sender/controller/GreetingController.java
@@ -1,14 +1,21 @@
 package com.programmer.greeting.sender.controller;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.client.RestTemplate;
+
 import com.programmer.greeting.exception.UserNotFoundException;
 import com.programmer.greeting.sender.beans.GreetingDetails;
 import com.programmer.greeting.sender.beans.ResponseMessgae;
@@ -17,6 +24,8 @@ import com.programmer.greeting.sender.service.GreetingSenderService;
 @CrossOrigin
 @RestController
 public class GreetingController {
+
+	private RestTemplate restTemplate;
 	@Autowired
 	private ResponseMessgae responseMessgae;
 	@Autowired
@@ -31,13 +40,13 @@ public class GreetingController {
 	}
 
 	@RequestMapping(value = "/getGreetingDetails/{id}", method = RequestMethod.GET)
-	public GreetingDetails getGreetingDetails(@PathVariable("id") Integer id) {	
+	public GreetingDetails getGreetingDetails(@PathVariable("id") Integer id) {
 		GreetingDetails greetingDetails = greetingService.getGreetingDetails(id);
 		if(greetingDetails == null){
 			throw new UserNotFoundException("id="+id);
 		}
 	return greetingDetails;
-	
+
 	}
 
 	@RequestMapping(value = "/getAllDetails", method = RequestMethod.GET)
@@ -45,7 +54,7 @@ public class GreetingController {
 		return greetingService.getAllDetails();
 
 	}
-	
+
 	@RequestMapping(value = "/deleteDetails/{id}", method = RequestMethod.DELETE)
 	public  ResponseMessgae deleteDetails(@PathVariable("id") Integer id) {
 		if (id != null) {
@@ -53,7 +62,7 @@ public class GreetingController {
 		}
 		return null;
 	}
-	
+
 	@RequestMapping(value = "/updateDetails", method = RequestMethod.PUT)
 	public  ResponseMessgae updateDetails(@RequestBody GreetingDetails detailsList) {
 		if (detailsList != null) {
@@ -61,5 +70,29 @@ public class GreetingController {
 		}
 		return null;
 	}
-	
+
+	@RequestMapping(value = "/getAllEvents", method = RequestMethod.GET)
+	public List<GreetingDetails>  getAllEvents()  {
+		List<GreetingDetails> eventList = new ArrayList<GreetingDetails>();
+		restTemplate = new RestTemplate();
+		ResponseEntity<List<GreetingDetails>> responseDetails = restTemplate.exchange(
+				"https://heroku-greeting-sender.herokuapp.com/getAllDetails", HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<GreetingDetails>>() {
+				});
+		System.out.println("Local Date= " + LocalDate.now());
+		List<GreetingDetails> greetingDetails = responseDetails.getBody();
+		greetingDetails.forEach(list -> {
+			LocalDate tempDate = LocalDate.parse(list.getDate());
+			if (tempDate.getDayOfMonth() == LocalDate.now().getDayOfMonth()
+					&& tempDate.getMonthValue() == LocalDate.now().getMonthValue()) {
+
+				eventList.add(list);
+
+			}
+
+		});
+		return eventList;
+
+	}
+
 }
